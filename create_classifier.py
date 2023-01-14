a57af7d1e2f972d7b9ffb47b75f2e7bb8e4423c0
Naqqash Latif <naqqash.latif@outlook.com>
2023-01-14 23:25:31 +0500

    First commit

---
+++ create_classifier.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image
+import os
+import cv2
+
+
+def train_classifer(name):
+    path = os.path.join(os.getcwd()+"/data/"+name+"/")
+
+    faces = []
+    ids = []
+    labels = []
+    pictures = {}
+
+    for root, dirs, files in os.walk(path):
+        pictures = files
+
+    for pic in pictures:
+
+        imgpath = path+pic
+        img = Image.open(imgpath).convert('L')
+        imageNp = np.array(img, 'uint8')
+        id = int(pic.split(name)[0])
+        faces.append(imageNp)
+        ids.append(id)
+
+    ids = np.array(ids)
+
+    clf = cv2.face.LBPHFaceRecognizer_create()
+    clf.train(faces, ids)
+    clf.write("./data/classifiers/"+name+"_classifier.xml")
